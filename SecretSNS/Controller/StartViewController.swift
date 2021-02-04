fd5ca44b5079983fadab2c6d4ffd478660cbc3ce
yonegen <yonegen0@gmail.com>
2021-02-04 15:42:55 +0900

    first commit

---
+++ SecretSNS/Controller/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  SecretSNS
+//
+//  Created by 米津純生 on 2021/02/01.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
